20e36681b5bae71033670a326168f53d50f5b38f
ladavrska <v_vrska@yahoo.com>
2019-10-07 16:08:43 +0200

    Initial commit

---
+++ NoteManagerCleanSwift/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NoteManagerCleanSwift
+//
+//  Created by Lada Vrska on 07/10/2019.
+//  Copyright © 2019 Lada Vrska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
